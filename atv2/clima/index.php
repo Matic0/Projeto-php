88edb36f7fd9166d7fa9d7736df7af6812f93d1e
Matic0 <57104088+Matic0@users.noreply.github.com>
2021-05-12 11:07:55 -0300

    Add files via upload

---
+++ atv2/clima/index.php
@@ -0,0 +1,36 @@
+<?php
+
+const ID = "AMeFGOqk86aEyL9yKSpgs";
+const SECRET = "aPG5tNMLvqz20lT6xUPj1lE8enRmzf6yjl9iaAmq";
+//https://api.aerisapi.com/[:endpoint]/[:action]?client_id=[ID]&client_secret=[SECRET]
+const BASE_URL = "https://api.aerisapi.com/conditions/";
+
+$teste = "";
+if (isset ($_GET['campo_cidade']) ) {
+    $teste = $_GET['campo_cidade'];
+}//elseif(null($_GET['campo_cidade'])){  $nome, $temperatura, $sensacao, $umidade,$velocidade  $dados = 1;} 
+else{print("xX_Nunca nem vi_Xx");}
+
+    $cidade = $_GET['campo_cidade'];
+    $cidade = urlencode($cidade);
+  $url = BASE_URL . "{$cidade},br?client_id=" . ID . "&client_secret=" . SECRET;
+echo $url;
+/*if(isset($_GET['campo_cidade'])){ */
+//file_get_contains($url);
+//echo $url;
+
+$json  = file_get_contents($url);
+$dados = json_decode($json, true);
+
+
+$nome = $dados['response'][0]['place']['name'];
+$temperatura = $dados['response'][0]['periods'][0]['tempC'];
+$sensacao = $dados['response'][0]['periods'][0]['feelslikeC'];
+$umidade = $dados['response'][0]['periods'][0]['humidity'];
+$velocidade = $dados['response'][0]['periods'][0]['windSpeedKPH'];
+
+// echo "<pre>";
+// print_r($dados['response'][0]['periods'][0]);
+// echo "</pre>";
+
+include "clima_view.php";
